241ad1363d22c9d1fbd71f1950cedf6f222c61e2
Unknown <bogin7@gmail.com>
2018-09-20 09:14:37 +0300

    pg host change

--- sqlite_nodejs/knexfile.js
+++ sqlite_nodejs/knexfile.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'sampledb-choose-a-date-server.7e14.starter-us-west-2.openshiftapps.com',
       user : 'admin',
       password : '12345678',
       database : 'sampledb'
