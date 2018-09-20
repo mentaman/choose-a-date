f1da449e78d833b8932d4ccfcb3e27743ce59301
Unknown <bogin7@gmail.com>
2018-09-20 05:34:54 +0300

    webpack middleware

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+require('./config/env');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const webpack = require('webpack');
+const middleware = require('webpack-dev-middleware');
+const config = require('./config/webpack.config.prod');
+let compiler = webpack(config);
 
 var indexRouter = require('./routes/index');
 var datesRouter = require('./routes/dates');
@@ -10,13 +17,16 @@ var datesRouter = require('./routes/dates');
 var app = express();
 
 // view engine setup
+app.use(middleware(compiler, {
+  publicPath: config.output.publicPath,  
+}));
 app.use(cors({origin: '*'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, './build')));
+//app.use(express.static(path.join(__dirname, './build')));
 app.use('/dates', datesRouter);
 
 // catch 404 and forward to error handler
