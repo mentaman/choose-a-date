27cc1490346f8b7863d37bbe824fad63ec8099ed
Unknown <bogin7@gmail.com>
2018-09-20 04:12:37 +0300

    united server & client

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, './build')));
 app.use('/dates', datesRouter);
 
 // catch 404 and forward to error handler
