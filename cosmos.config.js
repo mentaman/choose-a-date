d34e0d3ca88debef5e30b70f5df9ad8356a8231d
Unknown <bogin7@gmail.com>
2019-03-12 19:37:54 +0200

    css

---
+++ cosmos.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    webpackConfigPath: 'react-scripts/config/webpack.config',
+    publicPath: 'public',
+    proxiesPath: './cosmos.proxies',
+  };
--- src/containers/home/datepicker.js
+++ src/containers/home/datepicker.js
@@ -27,6 +27,40 @@ function getDates(startDate, stopDate) {
  export function getDateFormat(date) {
      return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
  }
+
+ export class DayDate extends Component {
+     getUsers() {
+        let {users, date} = this.props;
+        return (users && users[getDateFormat(date)]) || [];
+     }
+     render() {
+         let {date, choosed} = this.props;
+         let users = this.getUsers();
+        return (
+            <div key={getDateFormat(date)} 
+                onMouseDown={this.props.onMouseDown} 
+                onMouseEnter={this.props.onMouseEnter}
+                
+                 className={`day ${choosed ? "choosed" : "not-choosed"}`}>
+                    <div className="day-title">
+                        <div className={"day-name"}>
+                            יום {weekday[date.getDay()]}
+                        </div>
+                        <div className={"day-date"}>
+                            {date.getDate()}/{date.getMonth()+1}/{date.getFullYear()}
+                        </div>
+                    </div>
+                    <div className={"day-users"}>
+                        {users.map((user, idx) => (
+                            <div key={idx} className="day-users-user">
+                                <img src={`http://graph.facebook.com/${user}/picture?type=square`} />
+                            </div>
+                        ))}
+                    </div>
+                 </div>
+        )
+     }
+ }
 export class DatePicker extends Component {
     constructor(props) {
         super(props);
@@ -44,6 +78,7 @@ export class DatePicker extends Component {
     getDates = () => {
         return getDates(this.props.fromDate, this.props.toDate).map(getDateFormat).filter(date => this.isChosen((date)));
     }
+    
 
     render() {
         return <div onMouseUp={() => {
@@ -54,44 +89,37 @@ export class DatePicker extends Component {
         }} >
             {getDates(this.props.fromDate, this.props.toDate).map((date) => {
                 let choosed = this.isChosen(getDateFormat(date));
-                return (
-                <div key={getDateFormat(date)} onMouseDown={() => {
-                        console.log("down", date);
-                        let cleaning = false;
-                        let newDates = {...this.state.dates};
-                        if(choosed) {
-                            cleaning = true;
-                        }
-                        if(cleaning) {
-                            newDates[getDateFormat(date)] = false;
-                        } else {
-                            newDates[getDateFormat(date)] = true;
-                        }
-                        
-                        this.setState({dragging: true, cleaning, dates: newDates})
-                    }} 
-                    onMouseEnter={() => {
-                        if(this.state.dragging) {
-                            let newDates = {...this.state.dates};
-                            if(this.state.cleaning) {
-                                newDates[getDateFormat(date)] = false;
-                            } else {
-                                newDates[getDateFormat(date)] = true;
-                            }
-                            this.setState({dates: newDates})
-                            console.log("hover", date);
-                        }
-                    }}
-                    
-                     className={`day ${choosed ? "choosed" : "not-choosed"}`}>
-                        {date.getDate()}/{date.getMonth()+1}/{date.getFullYear()} - יום {weekday[date.getDay()]}
-                        {this.props.users && this.props.users[getDateFormat(date)] && this.props.users[getDateFormat(date)].map((user, idx) => (
-                            <div key={idx}>
-                                <img src={`http://graph.facebook.com/${user}/picture?type=square`} />
-                            </div>
-                        ))}
-                     </div>
-            )})}
+                return <DayDate choosed={choosed} 
+                                onMouseEnter={() => {
+                                    if(this.state.dragging) {
+                                        let newDates = {...this.state.dates};
+                                        if(this.state.cleaning) {
+                                            newDates[getDateFormat(date)] = false;
+                                        } else {
+                                            newDates[getDateFormat(date)] = true;
+                                        }
+                                        this.setState({dates: newDates})
+                                        console.log("hover", date);
+                                    }
+                                }}
+                                onMouseDown={() => {
+                                    console.log("down", date);
+                                    let cleaning = false;
+                                    let newDates = {...this.state.dates};
+                                    if(choosed) {
+                                        cleaning = true;
+                                    }
+                                    if(cleaning) {
+                                        newDates[getDateFormat(date)] = false;
+                                    } else {
+                                        newDates[getDateFormat(date)] = true;
+                                    }
+                                    
+                                    this.setState({dragging: true, cleaning, dates: newDates})
+                                }}
+                                users={this.props.users}
+                                date={date} />
+                })}
         </div>
     }
 }
