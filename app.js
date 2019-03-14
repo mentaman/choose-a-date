91ac20dc4bb29ca5da29215fb196717dba6f94cc
Unknown <bogin7@gmail.com>
2019-03-15 00:38:15 +0200

    build before

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+const env = 'production';
+process.env.NODE_ENV = env;
+process.env.BABEL_ENV = env;
 require('./config/env');
 var express = require('express');
 var path = require('path');
@@ -8,18 +9,20 @@ var logger = require('morgan');
 var cors = require('cors');
 const webpack = require('webpack');
 const middleware = require('webpack-dev-middleware');
-const config = require('./config/webpack.config.prod');
-let compiler = webpack(config);
 
 var indexRouter = require('./routes/index');
 var datesRouter = require('./routes/dates');
 
 var app = express();
-
+/*if(env.toLowerCase() === 'development') {
+  const config = require('./config/webpack.config.dev');
+  let compiler = webpack(config);
+  app.use(middleware(compiler, {
+    publicPath: config.output.publicPath,  
+  }));
+}*/
 // view engine setup
-app.use(middleware(compiler, {
-  publicPath: config.output.publicPath,  
-}));
+
 app.use(cors({origin: '*'}));
 app.use(logger('dev'));
 app.use(express.json());
@@ -28,6 +31,7 @@ app.use(cookieParser());
 
 //app.use(express.static(path.join(__dirname, './build')));
 app.use('/dates', datesRouter);
+app.use(express.static(path.join(__dirname, 'build')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
